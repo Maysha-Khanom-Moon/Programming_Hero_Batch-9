ab8275da4a25c2495e7cd38cc7340b4c04719f31
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 18:58:26 +0600

    nested if-else

--- Milestone 4/Module 17/if_else2.js
+++ Milestone 4/Module 17/if_else2.js
@@ -31,3 +31,24 @@ else if(price >= 2500) {
 else {
     console.log(price);
 }
+
+
+// Nested if-else
+const money = 500;
+
+if(money > 300) {
+    console.log('bro you are rich!!!');
+}
+else {
+    if(money > 100) {
+        console.log('tui gorib o na borolok o na');
+    }
+    else {
+        if(money > 0) {
+            console.log('dosto kola kha. calcium bara');
+        }
+        else {
+            console.log('tui amr bondhu nah');
+        }
+    }
+}
