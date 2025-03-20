95dd12550bfaf23779f3e16897f1638c157acc15
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 15:53:03 +0600

    naming style in js

---
+++ Milestone 4/Module 16/naming.js
@@ -0,0 +1,8 @@
+/**
+ *  ## Naming Style:
+ *   - camelCase: variables, functions
+ *   - PascalCase: classes, Constructed Functions
+ *   - SCREAMING_SNAKE_CASE: constants
+ * 
+ *   - snake_case: database column names
+ */
