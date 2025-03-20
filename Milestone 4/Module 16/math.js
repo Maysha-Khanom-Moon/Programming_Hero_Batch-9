b3f6c322dbc08062e67302cfc6021d40b99f4671
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 16:21:26 +0600

    math: number, operations, Math.functions

---
+++ Milestone 4/Module 16/math.js
@@ -0,0 +1,19 @@
+/**
+ ***********************************
+ *               MATH             
+ ***********************************
+ * 1. number: integer, float
+ *    - integer: +ve, -ve, 0
+ *    - float: 5.6, 2.2
+ * 
+ * 2. operators: +, -, *, /, %, ++, --
+ * 
+ * 3. Math object contains various functions:
+ *    - abs, round, floor, ceil, trunc
+ *    - pow, sqrt, cbrt
+ *    - min, max
+ *    - random
+ *    - sin, cos, tan, asin, acos, atan
+ *    - log, log10, exp
+ *    - PI, E
+ */
