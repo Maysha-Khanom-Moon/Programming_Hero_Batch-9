b9dc0d24755d25b76e6b26b8ef2e7fa3efed071c
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 17:03:02 +0600

    parse(left to right) and NaN

--- Milestone 4/Module 16/math.js
+++ Milestone 4/Module 16/math.js
@@ -30,11 +30,27 @@ var priceTag = '12';
 console.log(typeof priceTag);
 console.log(priceTag + 2); // 122
 
-// parseInt: string --> integer
-// parseFloat: string --> float
-priceTag = parseInt(priceTag);
-console.log(typeof priceTag);
-console.log(priceTag + 2); // 14
+/**
+ * ---------------------------------------------------
+ *                 parseInt, parseFloat 
+ * ---------------------------------------------------
+ * - string to number (left to right)
+ * -stop when it get any character other than number (for float consider . also)
+ * 
+ * - number (yellow)
+ * - string (white)
+ */
+
+var price = '12.5a.d';
+console.log(parseInt(price)); // 12
+
+var price = '125a';
+console.log(parseInt(price)); // 125
+console.log(parseFloat(price)); // 125
+
+var price = '124.2a'
+console.log(parseFloat(price)) // 124.2
+
 
 // round, floor, ceil, trunc: number --> integer
 var price = 12.5;
@@ -42,3 +58,14 @@ console.log('\n' + Math.round(price)); // 13
 console.log(Math.floor(price)); // 12   
 console.log(Math.ceil(price)); // 13
 console.log(Math.trunc(price)); // 12
+
+
+/**
+ *  ---- NaN: Not a Number ----
+ * - NaN is a value representor, not a data type
+ */
+console.log();
+
+var price = 'a125';
+console.log(parseInt(price)); // NaN
+console.log(typeof NaN); // number
