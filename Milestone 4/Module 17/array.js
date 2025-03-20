653d2347d5032325426ac91dd9f3eea49ad89998
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 19:56:59 +0600

    push, pop, length

--- Milestone 4/Module 17/array.js
+++ Milestone 4/Module 17/array.js
@@ -1,2 +1,17 @@
 const mixedArray = [12, 'Alahbad', true, 45.566];
+console.log(mixedArray);
 
+// length
+console.log("Length:", mixedArray.length);
+
+// push: add new value at the end
+mixedArray.push('new value');
+
+// add multiple value
+mixedArray.push('new value1', 'new value2');
+console.log(mixedArray);
+
+// pop: remove last value
+mixedArray.pop();
+mixedArray.pop();
+console.log(mixedArray);
