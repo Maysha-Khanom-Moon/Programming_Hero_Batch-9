969a9d66a8f136f1795e79de672e545f17cc64e7
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 16:09:49 +0600

    naming style and naming convention in js

--- Milestone 4/Module 16/naming.js
+++ Milestone 4/Module 16/naming.js
@@ -6,3 +6,15 @@
  * 
  *   - snake_case: database column names
  */
+
+
+/**
+ *  -------- Naming Conventions -------- 
+ *  1. no keyword
+ *  2. no space
+ *  3. no quotes
+ *  4. no special characters (except _, $)
+ *  5. can't start with a number
+ * 
+ *  - js is case sensitive language
+ */
