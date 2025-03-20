9d3b76d47f361a380987db7c2d4023c280fa7502
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 05:29:40 +0600

    get function in Date

--- Milestone 4/Module 21/date.js
+++ Milestone 4/Module 21/date.js
@@ -8,3 +8,11 @@ console.log(Date()); // current date and time
 // 2025-03-20T23:26:06.793Z
 console.log(new Date()); // current date and time
 console.log(new Date().toString(), '\n');
+
+
+// get
+const date = new Date('2062-10-19');
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
