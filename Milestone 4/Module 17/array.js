c258460b4a0e72f5ba39bb2980bb4e241eaecc74
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 20:11:08 +0600

    indexing, indexOf, lastIndexOf

--- Milestone 4/Module 17/array.js
+++ Milestone 4/Module 17/array.js
@@ -8,7 +8,7 @@ console.log("Length:", mixedArray.length);
 mixedArray.push('new value');
 
 // add multiple value
-mixedArray.push('new value1', 'new value2');
+mixedArray.push(12, 'new value1', 'new value2');
 console.log(mixedArray);
 
 // pop: remove last value
@@ -17,5 +17,23 @@ mixedArray.pop();
 console.log(mixedArray);
 
 // includes: check value is exist or not
+console.log('\nincludes: ');
 console.log(mixedArray.includes('new value')); // true
 console.log(mixedArray.includes('new value1')); // false
+
+
+// indexing: start from 0
+console.log('\nindexing:');
+console.log(mixedArray[0]); // 12
+
+/**
+ * indexOf(x): return index of x
+ * lastIndexOf(x): return last index of x
+ * ---> if x is not exist return -1
+ */
+console.log(mixedArray.indexOf(true)); // 2
+console.log(mixedArray.lastIndexOf(12)); // 5
+
+// indexOf(x, y): return index of x starting search from index-y
+console.log(mixedArray.indexOf(true, 2)); // 2
+console.log(mixedArray.indexOf(true, 3)); // -1
