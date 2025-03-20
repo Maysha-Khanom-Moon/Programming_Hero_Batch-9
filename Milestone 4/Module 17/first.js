d36078fbc01e98af1fa922f27bf373d9498c2c12
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 17:59:39 +0600

    and or logical not

--- Milestone 4/Module 17/first.js
+++ Milestone 4/Module 17/first.js
@@ -7,6 +7,18 @@
  * 5. less then or equal: <=
  * 6. not equal: !=
  * 
+ * -----------------------
+ * 7. and: &&
+ * 8. or: ||
+ * 
+ * -----------------------
+ * 9. Logical not: !
+ * --> !false: true
+ * --> !true: false
+ * 
+ * 10. double not: !!
+ * ==> !!false: false
+ * ==> !!true: true
  */
 
 console.log(5 > 10); // false
