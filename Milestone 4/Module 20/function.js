be4cc2b61b52b8dcc76ce64ce593038cb7d04cef
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 05:00:37 +0600

    function declaration, parameter vs argument

---
+++ Milestone 4/Module 20/function.js
@@ -0,0 +1,10 @@
+/**
+ * Parameters: variables declared in a function definition
+ * Arguments: actual values passed to the function (when call)
+ * 
+ * ----- FUNCTION DECLARATION ----
+ * function funcName(params) {
+ *      statement;
+ *      return; // optional
+ * }
+ */
