09be239e41c1251ed2b79c73ecf547cff08f76c2
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 19:01:20 +0600

    ternary operator

---
+++ Milestone 4/Module 17/ternary.js
@@ -0,0 +1,17 @@
+/**
+ * TERNARY --> three parts
+ * 
+ *  condition ? // when true : // when false;
+ */
+
+const age = 20;
+// normal if-else
+if(age >= 18) {
+    console.log('you can vote');
+}
+else {
+    console.log('Ghumai thako');
+}
+
+// ternary
+age >= 18 ? console.log('vote dio') : console.log('Ghumai Thako');
