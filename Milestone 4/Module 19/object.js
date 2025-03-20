17448cdea6fef17908d3f167e7068af15c2a9cbf
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 23:58:42 +0600

    structure of an object

---
+++ Milestone 4/Module 19/object.js
@@ -0,0 +1,13 @@
+/**
+ * ---- object ----
+ * 
+ * objectName = {
+ *      key: value,
+ *      key2: value2,
+ *      ...
+ *      ...
+ * }
+ * 
+ * ==> it's not sequential
+ * ==> it's non-primitive. Because, everyone is not same type.
+*/
