91f4b5b05c56dcb041c69d3149611024f1e15712
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 17:58:18 +0600

    comparison

---
+++ Milestone 4/Module 17/first.js
@@ -0,0 +1,20 @@
+/**
+ * ****** COMPARISON ******
+ * 1. bigger: >
+ * 2. less: <
+ * 3. equal: ==
+ * 4. greater than or equal: >=
+ * 5. less then or equal: <=
+ * 6. not equal: !=
+ * 
+ */
+
+console.log(5 > 10); // false
+console.log(5 < 10); // true
+console.log(5 == 10); // false
+console.log(10 == 10); // true
+console.log(15 >= 10); // true
+console.log(15 <= 10); // false
+console.log(10.1 == 10); // false
+console.log(10.1 != 10); // true
+
