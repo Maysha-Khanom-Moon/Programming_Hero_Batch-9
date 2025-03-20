9573b25cea8ec7bb893f62539d1da33e2fd30dad
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 20:24:36 +0600

    spread operator

---
+++ Milestone 4/Module 17/array2.js
@@ -0,0 +1,14 @@
+const num1 = [1, 43, 9, 3]
+const num2 = [4, 5, 6, 7]
+
+// spread operator (...)
+const num3 = [...num1, ...num2]; // merging
+console.log('\nspread operator: ');
+console.log(num3);
+
+const word = 'hello';
+wordArray = [...word];
+console.log(wordArray);
+
+
+//
