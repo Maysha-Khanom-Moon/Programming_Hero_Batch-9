75a945b499b1f22e7096376adf2a09ce9dd3ccd1
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 18:01:07 +0600

    check value and type both

--- Milestone 4/Module 17/first.js
+++ Milestone 4/Module 17/first.js
@@ -30,3 +30,14 @@ console.log(15 <= 10); // false
 console.log(10.1 == 10); // false
 console.log(10.1 != 10); // true
 
+
+/**
+ * == : just check the value
+ * === : check value and type both
+ */
+console.log("\ncheck value and type both: ");
+console.log(10 == '10'); // true
+console.log(10 === '10'); // false
+
+console.log(10 != '10'); // false
+console.log(10 !== '10'); // true
