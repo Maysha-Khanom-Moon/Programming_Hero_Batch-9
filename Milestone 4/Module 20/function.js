958888bc8180fd584abd1c4338111c09c34e8b7f
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 05:03:27 +0600

    2 way to function declaration

--- Milestone 4/Module 20/function.js
+++ Milestone 4/Module 20/function.js
@@ -1,10 +1,19 @@
 /**
  * Parameters: variables declared in a function definition
  * Arguments: actual values passed to the function (when call)
- * 
- * ----- FUNCTION DECLARATION ----
- * function funcName(params) {
- *      statement;
- *      return; // optional
- * }
  */
+
+// ----- FUNCTION DECLARATION ----
+// 1.
+function first () {
+    console.log('first');
+}
+
+// 2.
+let second = function () {
+    return 2;
+}
+
+// call
+first();
+console.log(second());
