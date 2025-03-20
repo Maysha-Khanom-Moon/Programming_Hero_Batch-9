748f30f3152b15c01187901c732c46c777a94ad1
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 04:42:26 +0600

    nested object

--- Milestone 4/Module 19/object.js
+++ Milestone 4/Module 19/object.js
@@ -62,3 +62,24 @@ const keys = Object.keys(bottle);
 for (const value of keys) {
     console.log(value + ': ' + bottle[value]);
 }
+console.log();
+
+
+
+// ------------- nested object -------------
+const college = {
+    name: 'vnc',
+    class: ['11', '12'],
+    events: ['science fair', 'bijoy dibos', '21 Feb'],
+    unique: {
+        color: 'blue',
+        result: {
+            gpa: 5,
+            merit: 'top'
+        }
+    }
+} 
+
+console.log(college.unique);
+console.log(college.unique.result);
+console.log(college.unique.result.gpa, '\n');
