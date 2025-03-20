572c58507f66c1c2346b6923a7bd1f4884911446
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 18:48:17 +0600

    if and if-else

---
+++ Milestone 4/Module 17/if_else.js
@@ -0,0 +1,30 @@
+/**
+ * ----------- STRUCTURE -----------
+ * if(condition) {
+ *      // condition true
+ * }
+ * 
+ * ---------------------------------
+ * if(condition) {
+ *      // condition true
+ * } 
+ * else {
+ *      // condition false
+ * }
+ * 
+*/
+
+// 1. only if
+if(3 < 10) {
+    console.log('I have smaller value');
+}
+
+// 2. if-else
+var biryaniPrice = 100;
+
+if(biryaniPrice < 100) {
+    console.log('Give me some biryani');
+}
+else {
+    console.log('Give me some Rice and Dal');
+}
