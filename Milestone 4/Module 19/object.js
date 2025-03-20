d0616ce52a97ce4f0919fcecdd8d056a442f3fc3
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 00:18:38 +0600

    3 way to declare an Object

--- Milestone 4/Module 19/object.js
+++ Milestone 4/Module 19/object.js
@@ -11,3 +11,26 @@
  * ==> it's not sequential
  * ==> it's non-primitive. Because, everyone is not same type.
 */
+
+// const only prevents reassigning the entire object, but object is mutable
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClean: false
+}
+
+// accessing value
+console.log(bottle.brand);
+console.log(bottle['brand'], '\n');
+
+
+// ------------- Declaring Object -------------
+// we can add key and values now or later
+// 1.
+const per1 = {}
+// 2.
+const per2 = new Object();
+// 3.
+const per3 = Object.create({}); 
+// to use this way: need a prototype object or null(means no prototype). But can't be empty
