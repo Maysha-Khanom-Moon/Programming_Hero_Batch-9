14518f000e218c71e902dcb6e93442e298fa9b1a
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 15:35:13 +0600

    5 things to declare a variable

---
+++ Milestone 4/Module 16/first.js
@@ -0,0 +1,31 @@
+/*
+    ---------- JS ---------
+- Brenden Eich (1995)
+- official name ecmascript
+- it's an interpreted language
+*/
+console.log("Hello World");
+
+/**
+ *  variable: container to store data. which container allocate some storage in memory based on data type.
+ * 
+ * 
+ * ### 5 things to declare a variable -
+ *      1. keywords (var/ let/ const)
+ *      2. variable name
+ *      3. '=' to assign value
+ *      4. variable value
+ *      5. ;
+ * 
+ * 
+ *  - js is a dynamically typed language
+ */
+
+var name = 'moon';
+var weight = 20;
+var age; // undefined
+
+const pi = 3.1415; // value can't be changed
+
+name = 'mkm'; // change the value
+console.log(name);
