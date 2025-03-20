e02c3f2f36942b5f33b084d2ce04e9a5067b9d8b
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 16:36:41 +0600

    typeof, parseInt, parseFloat, num --> int

--- Milestone 4/Module 16/math.js
+++ Milestone 4/Module 16/math.js
@@ -17,3 +17,28 @@
  *    - log, log10, exp
  *    - PI, E
  */
+
+var orangPrice = 10;
+
+//  typeof
+console.log('\n' + orangPrice);
+console.log(typeof orangPrice);
+console.log(typeof(orangPrice));
+
+
+var priceTag = '12';
+console.log(typeof priceTag);
+console.log(priceTag + 2); // 122
+
+// parseInt: string --> integer
+// parseFloat: string --> float
+priceTag = parseInt(priceTag);
+console.log(typeof priceTag);
+console.log(priceTag + 2); // 14
+
+// round, floor, ceil, trunc: number --> integer
+var price = 12.5;
+console.log('\n' + Math.round(price)); // 13
+console.log(Math.floor(price)); // 12   
+console.log(Math.ceil(price)); // 13
+console.log(Math.trunc(price)); // 12
