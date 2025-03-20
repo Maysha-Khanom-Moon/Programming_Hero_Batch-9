a94cd89273fbb308055c3a65cc68cc9bc273f055
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-20 22:05:49 +0600

    for loop

---
+++ Milestone 4/Module 18/loop.js
@@ -0,0 +1,18 @@
+/**
+ * LOOPING TECHNIQUE
+ * 1. for loop
+ * 2. while loop
+ * 3. do-while
+ * 4. for-of: array loop
+ * 5. for-in: object loop
+ */
+
+/**
+ * 1.
+ * for(initial, checking, update) {
+ *      .. statement ..
+ * }
+ */
+for(let num = 0; num < 5; num++) {
+    console.log(num);
+}
