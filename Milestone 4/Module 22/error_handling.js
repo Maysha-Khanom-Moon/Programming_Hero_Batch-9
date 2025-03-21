e44544bf3ff3325cfa60b7e33a9827b0c0352724
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 06:32:14 +0600

    add is concate but mutiply is not(NaN)

---
+++ Milestone 4/Module 22/error_handling.js
@@ -0,0 +1,11 @@
+// js: left to right
+// js follows PEMDAS (Parentheses, Exponents, Multiplication, Division, Addition, Subtraction)
+
+// add: (concatenation)
+console.log(2 + 2 + 'a'); // 4a
+console.log('a' + 2 + 2); // a22
+console.log(2 + 'a' + 2 + '\n'); // 2a2
+
+// multiplication
+console.log('a' * 2); // NaN
+console.log(2 * 'a'); // NaN
--- Milestone 4/Module 22/examples.js
+++ Milestone 4/Module 22/examples.js
@@ -1,4 +1,4 @@
-// shopping
+// shopping cart
 const products = [
     { name: 'shampoo', price: 300, quantity: 2},
     { name: 'chiruni', price: 100, quantity: 3},
@@ -12,4 +12,25 @@ function cartTotal(products) {
         sum += i.price * i.quantity;
     return sum;
 }
-console.log(cartTotal(products));
+console.log('Total: ' + cartTotal(products) + '\n');
+
+
+// cheapest one
+const mobiles = [
+    { name: 'Samsung', price: 20000, camera: '12mp', color: 'black' },
+    { name: 'Xoami', price: 18000, camera: '12mp', color: 'black' },
+    { name: 'Oppo', price: 30000, camera: '12mp', color: 'black' },
+    { name: 'Oppo', price: 10000, camera: '12mp', color: 'black' },
+    { name: 'Walton', price: 31000, camera: '12mp', color: 'black' },
+    { name: 'HTC', price: 27000, camera: '12mp', color: 'black' }
+]
+
+function getCheapest(phones) {
+    phones = Object.keys(phones);
+    let phone = phones[0];
+    for (const i of phones)
+        if(phone.price > i.price)
+            phone = phones[i];
+    return phone;
+}
+console.log('Cheapest one: ' + getCheapest(mobiles) + '\n');
