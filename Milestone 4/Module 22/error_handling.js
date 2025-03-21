2cca77e3cc9b98dc9240af8ebf1abdae54a973b9
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-21 06:35:44 +0600

    error handling in js

--- Milestone 4/Module 22/error_handling.js
+++ Milestone 4/Module 22/error_handling.js
@@ -7,5 +7,17 @@ console.log('a' + 2 + 2); // a22
 console.log(2 + 'a' + 2 + '\n'); // 2a2
 
 // multiplication
-console.log('a' * 2); // NaN
-console.log(2 * 'a'); // NaN
+console.log('a' * 2); // NaN (yellow)
+console.log(2 * 'a', '\n'); // NaN (white)
+
+
+// ----- ERROR HANDLING ------
+function multi(a, b) {
+    return isNaN(a * b) ? 'give a number' : a * b;
+}
+console.log(multi(5, 7)); // 35
+console.log(multi([5], 7)); // 35
+console.log(multi(5, '7')); // 35
+console.log(multi([5], '7')); // 35
+console.log(multi([5, 7], 8)); // give a number
+console.log(multi(5, 'seven'), '\n'); // give a number
