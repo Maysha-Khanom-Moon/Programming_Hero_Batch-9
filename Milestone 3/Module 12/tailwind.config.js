28e6e902ef79215e7058598cd4471f78fdc49da2
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-22 05:23:48 +0600

    normal format is best

--- Milestone 3/Module 12/tailwind.config.js
+++ Milestone 3/Module 12/tailwind.config.js
@@ -1,22 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Adjust based on your project structure
-  theme: {
-    extend: {
-      colors: {
-        primary: "linear-gradient(180deg, #A4BC46 0%, #85A019 100%)",
-        secondary: "#363958",
-      },
-    },
-  },
-  plugins: [],
-};
-
-
+// /** @type {import('tailwindcss').Config} */
 // module.exports = {
-//     content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
-//     theme: {
-//       extend: {},
+//   content: ["./src/**/*.{html,js,ts,jsx,tsx}"], // Adjust based on your project structure
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "linear-gradient(180deg, #A4BC46 0%, #85A019 100%)",
+//         secondary: "#363958",
+//       },
 //     },
-//     plugins: [],
-//   };
+//   },
+//   plugins: [],
+// };
+
+module.exports = {
+    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+};
