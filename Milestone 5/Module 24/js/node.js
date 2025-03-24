d20239810f9b8ee34ec24b33deb493064b4a309d
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-25 00:35:30 +0600

    childNodes

---
+++ Milestone 5/Module 24/js/node.js
@@ -0,0 +1,5 @@
+const Container = document.getElementById('places-container');
+console.log(Container.childNodes);
+console.log(Container.firstChild);
+console.log(Container.childNodes[3]);
+console.log(Container.lastChild.previousSibling);
