338a04089d768bf1894ae83204cb86fe1ac859d1
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-25 00:42:41 +0600

    createElement

--- Milestone 5/Module 24/js/node.js
+++ Milestone 5/Module 24/js/node.js
@@ -3,3 +3,11 @@ console.log(Container.childNodes);
 console.log(Container.firstChild);
 console.log(Container.childNodes[3]);
 console.log(Container.lastChild.previousSibling);
+console.log(Container.parentNode.parentNode.parentNode.parentNode); // null --> document --> html --> body --> main --> section
+
+
+// createElement
+const li = document.createElement('li');
+li.innerText = 'Dhaka';
+li.style.color = 'black';
+Container.childNodes[3].appendChild(li);
