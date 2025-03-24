499fe461d78cffe2e855eea765eee808ec11d399
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-24 17:48:10 +0600

    style in js

--- Milestone 5/Module 24/js/dom.js
+++ Milestone 5/Module 24/js/dom.js
@@ -4,7 +4,11 @@ console.log('\n');
 
 
 // dynamic style
+// css --> snake
+// js --> camel
 const importantPlaces = document.querySelectorAll('.important-places');
 for (const place of importantPlaces) {
-    place.style.color = 'red';
+    place.style.backgroundColor = 'cadetblue';
 }
+
+// 
