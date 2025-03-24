2997cd3265a27df006af348427547bb4974f0c19
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-24 17:58:07 +0600

    getAttribute by class, style

--- Milestone 5/Module 24/js/dom.js
+++ Milestone 5/Module 24/js/dom.js
@@ -6,9 +6,13 @@ console.log('\n');
 // dynamic style
 // css --> snake
 // js --> camel
-const importantPlaces = document.querySelectorAll('.important-places');
-for (const place of importantPlaces) {
+const Places = document.querySelectorAll('.important-places');
+for (const place of Places) {
     place.style.backgroundColor = 'cadetblue';
 }
 
-// 
+// getAttribute
+const title = document.getElementById('fruits-title');
+console.log(title.getAttribute('class'));
+console.log(title.getAttribute('style'));
+console.log('\n');
