4aab4288ae13184f1daf968c642e2bf3646dae60
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-24 16:29:33 +0600

    dynamic style

--- Milestone 5/Module 24/js/dom.js
+++ Milestone 5/Module 24/js/dom.js
@@ -1,2 +1,10 @@
 console.log("hello from js");
 console.log(document);
+console.log('\n');
+
+
+// dynamic style
+const importantPlaces = document.querySelectorAll('.important-places');
+for (const place of importantPlaces) {
+    place.style.color = 'red';
+}
