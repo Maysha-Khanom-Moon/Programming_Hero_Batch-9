85b1805269f490386f2c7aedb800433c8c1de30a
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-24 18:04:29 +0600

    classList: add, remove, toggle

--- Milestone 5/Module 24/js/dom.js
+++ Milestone 5/Module 24/js/dom.js
@@ -11,8 +11,23 @@ for (const place of Places) {
     place.style.backgroundColor = 'cadetblue';
 }
 
+
 // getAttribute
 const title = document.getElementById('fruits-title');
 console.log(title.getAttribute('class'));
 console.log(title.getAttribute('style'));
+console.log('\n');
+
+
+// classList
+let classListOfTitle = title.classList;
+console.log(classListOfTitle);
+
+classListOfTitle.add('red');
+classListOfTitle.add('green');
+classListOfTitle.remove('green');
+classListOfTitle.toggle('green'); // if not exist then add, if exist then remove
+
+classListOfTitle = title.classList; // not needed
+console.log(classListOfTitle);
 console.log('\n');
