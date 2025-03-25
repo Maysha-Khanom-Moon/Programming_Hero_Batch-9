017c783d28840fe335d5e50e8c385a8792b2513b
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-26 00:13:07 +0600

    event, event handler, and some common events

---
+++ Milestone 5/Module 25/js/event.js
@@ -0,0 +1,17 @@
+console.log('This is separate js file');
+
+// event: action of user or browser
+// event handler: function to handle the event
+
+// event handler --> on + event (small letters)
+
+/**
+ * ## some common events:
+ *  - click and mouse: click, mouseover, scroll
+ *  - keyboard: keydown, keyup
+ *  - form: submit, change, focus
+ *  - window, document: load, resize, scroll
+ *  - clipboard: copy, paste
+ *  - drag and drop: dragstart, drop
+ *  - media: play, pause
+ */
