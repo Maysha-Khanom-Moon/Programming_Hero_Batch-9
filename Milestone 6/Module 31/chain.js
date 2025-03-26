7e8a8b46ef903809c8ce5a9f195c73496a2e6fac
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-27 05:50:58 +0600

    chaining in nested object or array

---
+++ Milestone 6/Module 31/chain.js
@@ -0,0 +1,12 @@
+// data access
+const data = [{id: 1, name: 'abul', address: 'kochu khet'}]
+console.log(data[0].address);
+
+const products = {
+    count: 5000,
+    data: [
+        { id: 1, name: 'lenovo laptop', price: 65000},
+        { id: 2, name: 'macbook', price: 165000}
+    ]
+}
+console.log(products.data[1].price);
