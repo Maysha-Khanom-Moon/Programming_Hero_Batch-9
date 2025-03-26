0d139a0be28857903c0d9f59e2cf206984f6dcee
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-26 21:20:12 +0600

    all in one way to get input values

--- Milestone 5/Module 26/js/script.js
+++ Milestone 5/Module 26/js/script.js
@@ -1,9 +1,15 @@
 console.log('helo');
 
-const areaContainer = document.getElementById('area-container')
+const areaContainer = document.getElementById('area-container');
 
-// for node list or html collection --> loop must
-document.querySelectorAll('.calculate').addEventListener('click', function (event) {
-    console.log(event);
-    
+// Loop is necessary for NodeList
+document.querySelectorAll('.calculate').forEach(button => {
+    button.addEventListener('click', function(event) {
+        // Use previousElementSibling to avoid text node issues
+        // children to only access element nodes
+        const i1 = event.target.parentNode.previousElementSibling.children[2].children[0].children[0].value;
+        const i2 = event.target.parentNode.previousElementSibling.children[2].children[1].children[0].value;
+        
+        
+    });
 });
