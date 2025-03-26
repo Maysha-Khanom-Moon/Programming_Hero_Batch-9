508b69d6da2c55c5984ed4451c65cc84029a13e1
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-26 21:02:21 +0600

    for nodelist or htmlcollection --> loop must

---
+++ Milestone 5/Module 26/js/script.js
@@ -0,0 +1,9 @@
+console.log('helo');
+
+const areaContainer = document.getElementById('area-container')
+
+// for node list or html collection --> loop must
+document.querySelectorAll('.calculate').addEventListener('click', function (event) {
+    console.log(event);
+    
+});
