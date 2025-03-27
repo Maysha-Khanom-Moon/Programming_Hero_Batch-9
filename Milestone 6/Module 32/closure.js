50e92506637dc23897bdda8fe3d406a971058b12
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-27 23:56:44 +0600

    closure

--- Milestone 6/Module 32/closure.js
+++ Milestone 6/Module 32/closure.js
@@ -1,7 +1,29 @@
 /**
  * ----- HOISTING -----
  * - var and functions are moved to the top of the current scope before code execution
- * - but only var declaration is hoisted. so it stays undefined
+ * - but only var declaration is hoisted. so it stays undefined until reached the declaration point
  * 
  * - let and const are hoisted but remain in temporary dead zone (tdz) until reached the declaration point
  */
+
+
+/**
+ * ----- CLOSURE -----
+ * 1. a function is defined inside another function
+ * 2. inner function reference the variable of outer function
+ * 3. inner function is returned by the outer function
+ * 
+ * - used in: encapsulation, data privacy, function factory,  event handlers & callbacks
+ */
+
+function outer() {
+    let counter = 0;
+    return () => {
+        counter++;
+        return counter;
+    };
+}
+const inner = outer();
+console.log(inner()); // 1
+console.log(inner()); // 2
+console.log(inner()); // 3
