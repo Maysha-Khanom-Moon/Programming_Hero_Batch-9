380760884f19b27dddc26186063a1b79e9bfcec5
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-27 06:17:03 +0600

    forEach - select elements based on condition then returns an array

--- Milestone 6/Module 31/for-each.js
+++ Milestone 6/Module 31/for-each.js
@@ -1,6 +1,17 @@
-// ## forEach
-// same as map but no return
+/**
+ * ----------- forEach -----------
+ * - same as map but no return
+*/
 
 const nums = [1, 5, 6, 4, 15];
 const result = nums.forEach(n => console.log(n))
 console.log(result); // undefined
+
+
+/**
+ * ----- filter ------
+ * - select elements based on condition then returns an array
+ */
+const players = [75, 65, 67, 72, 55, 59, 72, 88];
+const selected = players.filter(p => p % 2 === 0);
+console.log(selected);
