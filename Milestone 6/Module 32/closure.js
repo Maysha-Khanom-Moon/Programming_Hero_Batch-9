060d2f191911cf0db7d10022d0e894c8e32a5e3e
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-27 23:40:32 +0600

    hoisting in js

---
+++ Milestone 6/Module 32/closure.js
@@ -0,0 +1,7 @@
+/**
+ * ----- HOISTING -----
+ * - var and functions are moved to the top of the current scope before code execution
+ * - but only var declaration is hoisted. so it stays undefined
+ * 
+ * - let and const are hoisted but remain in temporary dead zone (tdz) until reached the declaration point
+ */
