e8a1cb98c22e787d82119e22113d7b9d352d0ffa
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-27 06:19:40 +0600

    find - same as filter but just return 1st one

--- Milestone 6/Module 31/for-each.js
+++ Milestone 6/Module 31/for-each.js
@@ -11,7 +11,18 @@ console.log(result); // undefined
 /**
  * ----- filter ------
  * - select elements based on condition then returns an array
+ * - not found then empty array
  */
 const players = [75, 65, 67, 72, 55, 59, 72, 88];
 const selected = players.filter(p => p % 2 === 0);
 console.log(selected);
+
+
+/**
+ * ----- find -----
+ * - same as filter but just return the first one
+ * - not found then undefined
+ */
+
+const findSelected = players.find(n => n % 2 === 0);
+console.log(findSelected);
