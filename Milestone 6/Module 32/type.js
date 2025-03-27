44c74406b5a68f3a73143a80015ae2ed2f11581e
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-27 17:47:36 +0600

    truthy and falsy

---
+++ Milestone 6/Module 32/type.js
@@ -0,0 +1,15 @@
+/**
+ * -------------- Truthy --------------
+ * 1. true
+ * 2. any number(+ve, -ve) except 0
+ * 3. any string
+ * 4. empty object or object
+ * 5. empty array or array
+ * 
+ * -------------- Falsy --------------
+ * 1. false
+ * 2. number = 0
+ * 3. empty string = ''
+ * 4. undefined
+ * 5. null
+ */
