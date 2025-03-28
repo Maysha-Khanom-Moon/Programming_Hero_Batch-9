f7f003b55d942deb03acbe9436ee800926e32362
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-29 00:01:10 +0600

    search bar done

--- Milestone 6/Module 34/js/phone.js
+++ Milestone 6/Module 34/js/phone.js
@@ -2,7 +2,17 @@ const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
-}
+    // console.log(phones);
 
+    displayPhones(phones);
+}
 loadPhone();
+
+const displayPhones = phones => {
+    // console.log(phones);
+
+    phones.forEach(phone => {
+        console.log(phone);
+        
+    })
+}
