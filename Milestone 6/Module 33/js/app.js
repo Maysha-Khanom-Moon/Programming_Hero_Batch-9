34e1f14d2092a5eb17e88e43172b799429edaa2c
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-28 06:18:06 +0600

    add css style also

--- Milestone 6/Module 33/js/app.js
+++ Milestone 6/Module 33/js/app.js
@@ -21,7 +21,8 @@ function displayUsers(users) {
     const ul = document.getElementById('users');
     for (const user of users) {
         const li = document.createElement('li');
-        li.innerText = `Name: ${user.name} \nEmail: ${user.email}\n\n`;
+        li.innerText = `Name: ${user.name} \nEmail: ${user.email}`;
+        li.setAttribute('class', 'bg-green-800 p-4 my-3 rounded text-white font-bold');
         ul.appendChild(li);
     }
 }
