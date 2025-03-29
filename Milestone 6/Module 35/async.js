f3cf841ee6b0150f703d5487785f3ba65be342dc
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-03-30 05:23:15 +0600

    synchronous vs asynchronous

--- Milestone 6/Module 35/async.js
+++ Milestone 6/Module 35/async.js
@@ -1,9 +1,18 @@
-// synchronous follow the sequence and wait for other to finish
+// synchronous: follow the sequence and wait for other to finish
+// async: don't follow the sequence and don't wait for other
 
 console.log(1);
 doSomething();
 console.log(3);
 
+// setTimeout --> don't execute the function immediately
+setTimeout(doSomething, 1000);
+console.log(5);
+setTimeout(doSomething2);
+
 function doSomething() {
     console.log(2);
+}
+function doSomething2() {
+    console.log(6);
 }
