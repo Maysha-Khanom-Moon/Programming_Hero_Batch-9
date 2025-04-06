edcb0d985bace7387bca1920da4949b96b0932c4
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-06 17:14:00 +0600

    6 js fundamentals and es6 features

---
+++ Milestone 7/Module 37/recap.js
@@ -0,0 +1,14 @@
+/**
+ * ### 6 JS Fundamentals:
+ *  - variable declaration, conditional, loop, function, array, object
+ * 
+ * ### ES6 Features:
+ * - let, const
+ * - template literal, arrow function, destructuring
+ * - default parameter
+ * - spread operator (expand), rest operator (collect)
+ * - object literals, shorthand property
+ * - classes
+ * - promise, modules (import/ export)
+ */
+
