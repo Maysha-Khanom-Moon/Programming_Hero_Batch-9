d23b8d31821b9ba24bba0b17cad995af912fb805
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-07 16:52:32 +0600

    first dynamic component in jsx

--- Milestone 7/Module 38/explore-react/src/App.jsx
+++ Milestone 7/Module 38/explore-react/src/App.jsx
@@ -8,8 +8,17 @@ function App() {
     // <> </> ---> fragment tag
     <>
       <h1>Vite + React</h1>
+      <Person></Person>
     </>
   )
+}
+
+function Person() {
+  const person = {
+    name: 'John',
+    age: 30
+  }
+  return <h3>I am {person.name} and my age is {person.age}</h3>
 }
 
 export default App
