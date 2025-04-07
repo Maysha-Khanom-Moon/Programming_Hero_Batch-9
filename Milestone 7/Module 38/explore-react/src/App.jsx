5926db8281659b2a7414a6f73fad33022847ee79
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-07 17:05:48 +0600

    multiline return jsx

--- Milestone 7/Module 38/explore-react/src/App.jsx
+++ Milestone 7/Module 38/explore-react/src/App.jsx
@@ -3,12 +3,17 @@
 // import viteLogo from '/vite.svg'
 import './App.css'
 
+/* 
+ * <> </> ---> fragment tag
+ * 
+ * - this is not html, this is jsx
+ */
 function App() {
   return (
-    // <> </> ---> fragment tag
     <>
       <h1>Vite + React</h1>
       <Person></Person>
+      <Student></Student>
     </>
   )
 }
@@ -19,6 +24,20 @@ function Person() {
     age: 30
   }
   return <h3>I am {person.name} and my age is {person.age}</h3>
+}
+
+function Student() {
+  const student = {
+    name: 'Moon',
+    age: 22
+  }
+  return (
+    <div className="student">
+      <h3>This is a student.</h3>
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+    </div>
+  )
 }
 
 export default App
