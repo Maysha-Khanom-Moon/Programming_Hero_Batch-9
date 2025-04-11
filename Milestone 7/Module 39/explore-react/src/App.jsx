f1d5e51529bac19729cc974dcd4d4e33c14eb397
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-11 14:18:06 +0600

    3 ways for event handling

--- Milestone 7/Module 39/explore-react/src/App.jsx
+++ Milestone 7/Module 39/explore-react/src/App.jsx
@@ -2,14 +2,23 @@ import './App.css'
 
 function App() {
 
+  // component: capitalize
+  // in general function: camelCase
+
+  // by convention: add 'handle' word before event handler
   function handleClick() {
     alert('button clicked');
   }
+
+  const addToFive = (num) => alert(num + 5);
 
   return (
     <>
       <h2>React Core Concept 2</h2>
       <button onClick={handleClick}>Click Me</button>
+      <button onClick={() => alert('button 2 clicked')}>Click Me 2</button>
+      {/* don't call immediately */}
+      <button onClick={() => addToFive(10)}>addToFive</button>
     </>
   )
 }
