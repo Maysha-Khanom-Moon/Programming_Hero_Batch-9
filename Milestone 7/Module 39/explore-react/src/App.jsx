c8a0d0debc7df70aa363641758ca6512a6a342d9
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-12 14:10:06 +0600

    useEffect with useState

--- Milestone 7/Module 39/explore-react/src/App.jsx
+++ Milestone 7/Module 39/explore-react/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Counter from './Counter'
 import Team from './Team';
+import Users from './Users';
 
 function App() {
 
@@ -17,6 +18,8 @@ function App() {
   return (
     <>
       <h2>React Core Concept 2</h2>
+      <Users></Users>
+
       <Team></Team>
 
       <Counter></Counter>
