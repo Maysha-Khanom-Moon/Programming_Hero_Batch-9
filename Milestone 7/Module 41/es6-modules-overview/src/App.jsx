5c8ac63c177868618642fc6407f6e54a91da4886
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-20 15:51:27 +0600

    external api by github

--- Milestone 7/Module 41/es6-modules-overview/src/App.jsx
+++ Milestone 7/Module 41/es6-modules-overview/src/App.jsx
@@ -3,27 +3,37 @@ import './App.css'
 import Watch from './components/Watch/Watch';
 
 function App() {
+  // we can generate fake data using json generator or AI
+  
+  // 1. in memory object
+  /* 
+  const Watches = [
+      {id: 1, name: 'Rolex', price: 120000},
+      {id: 2, name: 'Casio', price: 45000},
+      {id: 3, name: 'Tissot', price: 60000},
+      {id: 4, name: 'Orient', price: 150000},
+      {id: 5, name: 'Apple', price: 19000},
+      {id: 6, name: 'Seiko', price: 50000},
+      {id: 7, name: 'Omega', price: 200000},
+      {id: 8, name: 'Samson', price: 70000}
+  ] 
+  */
+
+
   const [Watches, setWatches] = useState([]);
 
   useEffect( () => {
-    // local folder api (in public folder)
-    fetch('watches.json')
+    // 2. local folder api ~ inside public folder
+    // fetch('watches.json')
+
+    // 3. external api by github
+    // github json file --> raw file --> get the url
+    fetch('https://raw.githubusercontent.com/Maysha-Khanom-Moon/local_api/refs/heads/main/watches.json')
     .then(res => res.json())
     .then(data => setWatches(data))
   }, [])
 
-  // we can generate fake data using json generator or AI
-  // in memory object
-  //   const Watches = [
-  //     {id: 1, name: 'Rolex', price: 120000},
-  //     {id: 2, name: 'Casio', price: 45000},
-  //     {id: 3, name: 'Tissot', price: 60000},
-  //     {id: 4, name: 'Orient', price: 150000},
-  //     {id: 5, name: 'Apple', price: 19000},
-  //     {id: 6, name: 'Seiko', price: 50000},
-  //     {id: 7, name: 'Omega', price: 200000},
-  //     {id: 8, name: 'Samson', price: 70000}
-  // ]
+  
 
   return (
     <>
