9c4972148d4be347bea550948bad94b7f4962b03
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-29 15:11:35 +0600

    local image using

--- Milestone 7/Module 42/knowledge-cafe/src/components/Header.jsx
+++ Milestone 7/Module 42/knowledge-cafe/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import profile from '../assets/profile.jpg'
 
 function Header() {
   return (
     <div className='flex items-center justify-between mt-12 mb-12'>
         <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
 
-        <img className='w-14 h-14 rounded-full' src="https://randomuser.me/api/portraits/women/17.jpg" alt="" />
+        <img className='w-14 h-14 rounded-full' src={profile} alt="" />
     </div>
   )
 }
