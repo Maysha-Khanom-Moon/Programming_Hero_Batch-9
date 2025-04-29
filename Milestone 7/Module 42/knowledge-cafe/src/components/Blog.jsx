8a1676fa38aadd34a65c5de94389d0e50d62d401
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-04-29 20:30:41 +0600

    Blog template done

--- Milestone 7/Module 42/knowledge-cafe/src/components/Blog.jsx
+++ Milestone 7/Module 42/knowledge-cafe/src/components/Blog.jsx
@@ -1,11 +1,29 @@
 import PropTypes from 'prop-types';
 
 function Blog({blog}) {
-    console.log(blog);
+    const {cover, title, author, author_img, posted_date, reading_time, hashtags} = blog;
 
     return (
-        <div>
-        
+        <div className="flex flex-col border-b-[1px] border-gray-300 pb-4 gap-3">
+            <img className='rounded-md max-h-96' src={cover} alt="" />
+            <div className='flex justify-between items-center'>
+                <div className='flex justify-between items-center gap-4'>
+                    <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
+                    <div className='flex flex-col '>
+                        <h3 className='font-bold text-2xl'>{author}</h3>
+                        <p className='text-[#11111199] font-semibold text-base'>{posted_date}</p>
+                    </div>
+                </div>
+                <p className='text-[#11111199]'>{reading_time} min read</p>
+            </div>
+            <h1 className='text-4xl font-bold'>{title}</h1>
+
+            <div className='flex gap-4 text-[#11111199] text-xl font-medium'>
+                {
+                    hashtags.map(hashtag => <p>{hashtag}</p>)
+                }
+            </div>
+            <button className='text-blue-600 underline hover:text-blue-800 flex justify-start'>Mark as read</button>
         </div>
     )
 }
--- Milestone 7/Module 42/knowledge-cafe/src/components/Blogs.jsx
+++ Milestone 7/Module 42/knowledge-cafe/src/components/Blogs.jsx
@@ -14,9 +14,11 @@ function Blogs() {
 
   return (
     <div>
-        {
-          Blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
-        }
+        <div className='w-2/3 flex lg:flex-col flex-row gap-6'>
+          {
+              Blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+          }
+        </div>
     </div>
   )
 }
