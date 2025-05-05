9461b970081173ea498476cd456d49cd2c9c127d
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-05 18:54:32 +0600

    also added to Line chart: XAxis, YAxis, Tooltip

--- Milestone 8/Module 44/price-points/src/components/LineChart1/LineChart1.jsx
+++ Milestone 8/Module 44/price-points/src/components/LineChart1/LineChart1.jsx
@@ -1,25 +1,31 @@
-import { LineChart, Line } from "recharts";
+import { LineChart, Line, XAxis, YAxis, Tooltip } from "recharts";
 // if component name become same as library names, we can rename them like this
 // import { LineChart as LChart, Line } from "recharts";
 
 function LineChart1() {
   const data = [
-    { name: "Student 1", math: 78 },
-    { name: "Student 2", math: 85 },
-    { name: "Student 3", math: 62 },
-    { name: "Student 4", math: 90 },
-    { name: "Student 5", math: 73 },
-    { name: "Student 6", math: 88 },
-    { name: "Student 7", math: 95 },
-    { name: "Student 8", math: 67 },
-    { name: "Student 9", math: 80 },
-    { name: "Student 10", math: 76 },
+    { id: 1, name: "Ayaan Rahman", math: 78, physics: 72, chemistry: 81 },
+    { id: 2, name: "Nusrat Jahan", math: 85, physics: 89, chemistry: 76 },
+    { id: 3, name: "Tanvir Alam", math: 62, physics: 68, chemistry: 64 },
+    { id: 4, name: "Maliha Noor", math: 90, physics: 92, chemistry: 88 },
+    { id: 5, name: "Rayhan Kabir", math: 73, physics: 70, chemistry: 77 },
+    { id: 6, name: "Sumaiya Hossain", math: 88, physics: 85, chemistry: 90 },
+    { id: 7, name: "Rafiul Islam", math: 95, physics: 91, chemistry: 93 },
+    { id: 8, name: "Mehjabin Anwar", math: 67, physics: 69, chemistry: 66 },
+    { id: 9, name: "Zubair Ahmed", math: 80, physics: 78, chemistry: 74 },
+    { id: 10, name: "Tania Akter", math: 76, physics: 73, chemistry: 79 },
   ];
+
 
   return (
     <div className="m-10 border-2 w-fit">
-      <LineChart width={500} height={300} data={data}>
-        <Line type="monotone" dataKey="math" stroke="#8884d8" strokeWidth={2} />
+      <LineChart width={800} height={300} data={data}>
+        <XAxis dataKey="id" stroke="#8884d8" />
+        <YAxis stroke="#8884d8" />
+        <Tooltip />
+        <Line dataKey="math" stroke="#8884d8" strokeWidth={2} />
+        <Line dataKey="physics" stroke="#82ca9d" strokeWidth={2} />
+        <Line dataKey="chemistry" stroke="#ff7300" strokeWidth={2} />
       </LineChart>
     </div>
   );
