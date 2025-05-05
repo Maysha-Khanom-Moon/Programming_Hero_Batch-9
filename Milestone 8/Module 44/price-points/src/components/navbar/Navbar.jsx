bac363c732998f92bc83f30d258dd9c2ac46f842
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-05 13:38:41 +0600

    react icons and toggle button added

--- Milestone 8/Module 44/price-points/src/components/navbar/Navbar.jsx
+++ Milestone 8/Module 44/price-points/src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import Link from "../link/Link";
+import { IoMenuOutline as Menu } from "react-icons/io5";
 
 function Navbar() {
   const routes = [
@@ -9,9 +11,14 @@ function Navbar() {
     { id: 5, name: "Not Found", path: "*" },
   ];
 
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav>
-      <ul className="flex">
+    <nav className="mx-10 my-5">
+      <div onClick={() => setOpen(!open)} className="md:hidden cursor-pointer btn btn-square bg-white text-2xl border-1 mr-10">
+        <Menu />
+      </div>
+      <ul className="md:flex hidden">
         {routes.map((route) => (
           <Link key={route.id} route={route}></Link>
         ))}
