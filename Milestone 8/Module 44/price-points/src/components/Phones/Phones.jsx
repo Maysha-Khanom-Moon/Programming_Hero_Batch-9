419595d7d299fd007c51b48b2718b15c58cb0b20
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-05 20:39:06 +0600

    barchart by rechart for phones

--- Milestone 8/Module 44/price-points/src/components/Phones/Phones.jsx
+++ Milestone 8/Module 44/price-points/src/components/Phones/Phones.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
 import { useEffect, useState } from "react"
 
 function Phones() {
@@ -11,15 +12,37 @@ function Phones() {
         //   .then((data) => setPhones(data.data));
 
         axios.get("https://openapi.programming-hero.com/api/phones?search=iphone")
-            .then(data => setPhones(data.data.data))
+            .then(res => {
+                const phonesData = res.data.data;
 
+                // intercepting the data
+                const phonesWithFakeData = phonesData.map(phone => {
+                    const obj = {
+                        name: phone.phone_name,
+                        price: parseInt(phone.slug.split("-")[1]),
+                    }
+
+                    return obj;
+                })
+
+                console.log(phonesWithFakeData);
+                setPhones(phonesWithFakeData);
+            })
+            
     }, [])
 
     return (
-        <div className="mx-10 mb-20">
-            <h2 className="text-4xl font-bold">Phones: {phones.length}</h2>
-        </div>
-    )
+      <div className="mx-10 mb-20">
+        <h2 className="text-4xl font-bold">Phones: {phones.length}</h2>
+
+        <BarChart width={600} height={400} data={phones}>
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="price" fill="#8884d8" />
+        </BarChart>
+      </div>
+    );
 }
 
 export default Phones
