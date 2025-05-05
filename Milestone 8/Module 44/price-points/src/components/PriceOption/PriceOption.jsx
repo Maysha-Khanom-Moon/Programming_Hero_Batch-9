a7b546ee3b3eab86064e4b8c5f5284b86b7019bd
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-05 17:18:48 +0600

    card of price options ready

--- Milestone 8/Module 44/price-points/src/components/PriceOption/PriceOption.jsx
+++ Milestone 8/Module 44/price-points/src/components/PriceOption/PriceOption.jsx
@@ -2,8 +2,22 @@ import PropTypes from 'prop-types'
 
 function PriceOption({ option }) {
   return (
-    <div>
-      <h1>{option.name}</h1>
+    <div className="py-10 p-4 rounded-lg shadow-lg bg-black hover:bg-green-950 transition-bg duration-300 text-white cursor-pointer text-center flex flex-col justify-center items-center gap-5">
+      <h2 className="text-2xl font-bold">{option.name}</h2>
+      <p className="text-xl">
+        {option.price} / <span className="text-sm">{option.duration}</span>
+      </p>
+
+      <div className='ul  text-left'>
+        {
+          option.features.map((feature, index) => (
+            <li key={index} className="text-md text-gray-300">
+              {feature}
+            </li>
+          )
+        )
+        }
+      </div>
     </div>
   );
 }
--- Milestone 8/Module 44/price-points/src/components/PriceOptions/PriceOptions.jsx
+++ Milestone 8/Module 44/price-points/src/components/PriceOptions/PriceOptions.jsx
@@ -12,7 +12,7 @@ function PriceOptions() {
       }, []);
   
     return (
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mx-10">
         {
           priceOptions.map((option) => (
             <PriceOption
