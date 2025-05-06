b13609c6a6a72c4585124472c4632176ee227116
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-06 22:58:47 +0600

    form handling by ref with useRef

--- Milestone 8/Module 46/form-master/src/App.jsx
+++ Milestone 8/Module 46/form-master/src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
+import RefForm from './components/RefForm/RefForm';
 // import SimpleForm from './components/SimpleForm/SimpleForm'
-import StatefulForm from './components/StatefulForm/StatefulForm';
+// import StatefulForm from './components/StatefulForm/StatefulForm';
 
 function App() {
-
-
 
   return (
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm /> */}
 
-      <StatefulForm />
+      {/* <StatefulForm /> */}
+
+      <RefForm />
     </>
   )
 }
---
+++ Milestone 8/Module 46/form-master/src/components/RefForm/RefForm.jsx
@@ -0,0 +1,32 @@
+import { useRef } from "react";
+
+
+function RefForm() {
+
+    const nameRef = useRef(null);
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(nameRef.current.value);
+        console.log(emailRef.current.value);
+        console.log(passwordRef.current.value);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} action="">
+                <input ref={nameRef} type="text" name='name' placeholder="Name" />
+                <br />
+                <input ref={emailRef} type="email" name='email' placeholder="Email" />
+                <br />
+                <input ref={passwordRef} type="password" name='password' placeholder="Password" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    )
+}
+
+export default RefForm
