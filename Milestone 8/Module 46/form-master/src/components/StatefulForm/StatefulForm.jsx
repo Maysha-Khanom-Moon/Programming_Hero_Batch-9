72023bc1502ab8015d86ccb598922e40aee96340
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-06 22:23:34 +0600

    form with state to get value and set default value

--- Milestone 8/Module 46/form-master/src/components/StatefulForm/StatefulForm.jsx
+++ Milestone 8/Module 46/form-master/src/components/StatefulForm/StatefulForm.jsx
@@ -1,18 +1,37 @@
+import { useState } from "react";
 
 function StatefulForm() {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const handleSubmit = e => {
         e.preventDefault();
+        console.log(name, email, password);
     };
+
+    const handleNameChange = (e) => {
+      setName(e.target.value);
+    };
+
+    const handleEmailChange = e => {
+        setEmail(e.target.value);
+        console.log(email);
+    }
+
+    const handlePasswordChange = e => {
+        setPassword(e.target.value);
+    }
 
     return (
         <div>
         <form onSubmit={handleSubmit} action="">
-            <input type="text" name='name' placeholder='Name' />
+            <input onChange={handleNameChange} type="text" name='name' placeholder='Name' required />
             <br />
-            <input type="text" name='email' placeholder='Email' />
+            <input onChange={handleEmailChange} type="text" name='email' placeholder='Email' />
             <br />
-            <input type="password" name="password" placeholder='Password' />
+            <input onChange={handlePasswordChange} type="password" name="password" placeholder='Password' />
             <br />
             <button>Submit</button>
         </form>
