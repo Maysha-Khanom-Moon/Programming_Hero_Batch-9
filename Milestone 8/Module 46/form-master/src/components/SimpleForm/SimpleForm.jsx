043fa31b9a170e8863d96031f455f996e755aa52
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-06 21:57:48 +0600

    handle form with onSubmit and access field data

--- Milestone 8/Module 46/form-master/src/components/SimpleForm/SimpleForm.jsx
+++ Milestone 8/Module 46/form-master/src/components/SimpleForm/SimpleForm.jsx
@@ -5,13 +5,18 @@ function SimpleForm() {
     const handleSubmit = e => {
         // prevent default behavior of form submit (which is to refresh the page)
         e.preventDefault();
+
+        console.log("Name:", e.target.name.value);
+        console.log("Email:", e.target.email.value);
         console.log("form submitted");
     };
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" />
+                <input type="text" name='name' />
+                <br />
+                <input type="text" name='email' />
                 <br />
                 <button>Submit</button>
             </form>
