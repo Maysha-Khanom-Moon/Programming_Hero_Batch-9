48e23b59385ad7981840cf942199c35274b2085f
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-07 02:34:09 +0600

    more shortcut way to handle custom hook

--- Milestone 8/Module 46/form-master/src/components/HookForm/HookForm.jsx
+++ Milestone 8/Module 46/form-master/src/components/HookForm/HookForm.jsx
@@ -3,19 +3,20 @@ import useInputState from '../../hooks/useInputState';
 
 function HookForm() {
 
-    const [name, handleNameChange] = useInputState('Moon');
+    // const [name, handleNameChange] = useInputState('Moon');
+    const emailState = useInputState('moon@me.com');
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(name);
+        console.log(emailState);
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input value={name} onChange={handleNameChange} type="text" name='name' />
+                {/* <input value={name} onChange={handleSubmit} type="text" name='name' /> */}
                 <br />
-                <input type="email" name='email' />
+                <input {...emailState} type="email" name='email' />
                 <br />
                 <input type="password" name='password' />
                 <br />
