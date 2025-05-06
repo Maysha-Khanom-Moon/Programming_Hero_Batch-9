d0b8f43847f4d804fa34779dfa40300fd93c7901
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-06 23:36:31 +0600

    initially focus an input by useRef

--- Milestone 8/Module 46/form-master/src/components/RefForm/RefForm.jsx
+++ Milestone 8/Module 46/form-master/src/components/RefForm/RefForm.jsx
@@ -1,32 +1,46 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 
 function RefForm() {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
 
-    const nameRef = useRef(null);
-    const emailRef = useRef(null);
-    const passwordRef = useRef(null);
+  useEffect(() => {
+    nameRef.current.focus();
+  }, []);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        console.log(nameRef.current.value);
-        console.log(emailRef.current.value);
-        console.log(passwordRef.current.value);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(passwordRef.current.value);
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit} action="">
-                <input ref={nameRef} type="text" name='name' placeholder="Name" />
-                <br />
-                <input ref={emailRef} type="email" name='email' placeholder="Email" />
-                <br />
-                <input ref={passwordRef} type="password" name='password' placeholder="Password" />
-                <br />
-                <input type="submit" value="Submit" />
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handleSubmit} action="">
+        <input ref={nameRef} type="text" name="name" placeholder="Name" />
+        <br />
+        <input
+          ref={emailRef}
+          defaultValue={"moon@me.com"}
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <br />
+        <input
+          ref={passwordRef}
+          type="password"
+          name="password"
+          placeholder="Password"
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 }
 
 export default RefForm
