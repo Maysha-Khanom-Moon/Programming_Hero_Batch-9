4bc51b9355c36e6c32f3ff294e2dddfcb516087c
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-05-06 19:08:53 +0600

    show details link added

--- Milestone 8/Module 45/introduction-to-react-router/src/components/Posts/Post.jsx
+++ Milestone 8/Module 45/introduction-to-react-router/src/components/Posts/Post.jsx
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom';
 
 function Post({post}) {
   return (
-    <div className=''>
+    <div
+      style={{
+        border: "1px solid #ccc",
+        borderRadius: "5px",
+        padding: "10px",
+        margin: "10px 0",
+        backgroundColor: "skyblue",
+      }}
+    >
+        <h2>Post ID: {post.id}</h2>
         <h3>{post.title}</h3>
+        <Link to={`/posts/${post.id}`}>Show Details</Link>
     </div>
-  )
+  );
 }
 
 Post.propTypes = {
