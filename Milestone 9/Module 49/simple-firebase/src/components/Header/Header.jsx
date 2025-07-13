9277ed3c0edf21ad268c3893c5dc6100ccea40d7
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-07-13 22:18:52 +0600

    Outlet added

---
+++ Milestone 9/Module 49/simple-firebase/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <div>
+        <h2>This is Header</h2>
+    </div>
+  )
+}
--- Milestone 9/Module 49/simple-firebase/src/main.jsx
+++ Milestone 9/Module 49/simple-firebase/src/main.jsx
@@ -2,12 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import Main from './components/layouts/Main';
+import Home from './components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ]
   },
 ]);
 
