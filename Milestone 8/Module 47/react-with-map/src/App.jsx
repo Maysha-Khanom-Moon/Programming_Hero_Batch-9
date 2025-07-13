86e54cd0d00df26baf922da8968a982762a030af
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-07-13 21:06:26 +0600

    react icons

--- Milestone 8/Module 47/react-with-map/src/App.jsx
+++ Milestone 8/Module 47/react-with-map/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import GoogleMapReact from "google-map-react";
+import { FaHouseUser } from "react-icons/fa";
+
+const AnyReactComponent = ({ text }) => <div style={{ color: "white" }}>{text}</div>;
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultProps = {
+    center: {
+      lat: 24.374,
+      lng: 88.6011,
+    },
+    zoom: 11,
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      // Important! Always set the container height explicitly
+      <div style={{ height: "50vh", width: "100%" }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "" }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent lat={defaultProps.center.lat} lng={defaultProps.center.lng} text={<FaHouseUser />} />
+        </GoogleMapReact>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
