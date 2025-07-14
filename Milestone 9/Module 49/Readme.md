a0c2dae925b80ec7c19aa3150f39e867002e4335
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-07-14 15:49:33 +0600

    firebase sdk setup

--- Milestone 9/Module 49/Readme.md
+++ Milestone 9/Module 49/Readme.md
@@ -2,3 +2,9 @@
 Authentication verifies the identity of a user or entity, while authorization determines what resources or actions the authenticated user is permitted to access or perform. 
 
 In simpler terms, authentication answers the question "Who are you?", and authorization answers the question "What are you allowed to do?". 
+
+### Firebase SDK setup
+1. npm install firebase
+2. go to firebase and create a project
+3. register project
+4. get firebase config and do not save config on the client side
---
+++ Milestone 9/Module 49/simple-firebase/src/firebase/firebase.init.js
@@ -0,0 +1,21 @@
+// Do not store config on the client side
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBFEfo0DMxY3hGMUa6Z2cH5vtbeVXQ3bzQ",
+  authDomain: "simple-firebase-9be0b.firebaseapp.com",
+  projectId: "simple-firebase-9be0b",
+  storageBucket: "simple-firebase-9be0b.firebasestorage.app",
+  messagingSenderId: "818185972095",
+  appId: "1:818185972095:web:d929afa1c4f80ced52924a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
