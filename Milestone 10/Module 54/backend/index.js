7eed5f998c67f727cad0e1cb9eaee1c1d5572736
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-07-15 12:42:13 +0600

    backend setup

--- Milestone 10/Module 54/backend/index.js
+++ Milestone 10/Module 54/backend/index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const users = [
+    { id: 1, name: 'Sabana', email: 'sabana@bd.com' },
+    { id: 2, name: 'Sadia', email: 'sadia@bd.com' },
+    { id: 3, name: 'Sakina', email: 'sakina@bd.com' }
+]
+
 app.get('/', (req, res) => {
   res.send('User Management Server is running')
+})
+
+app.get('/users', (req, res) => {
+  res.send(users)
 })
 
 app.listen(port, () => {
