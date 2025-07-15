3524a737051414f110ef09abdf4ff37d772eb5ec
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-07-15 18:46:58 +0600

    backend setup

---
+++ Milestone 10/Module 55/simple-crud-server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
