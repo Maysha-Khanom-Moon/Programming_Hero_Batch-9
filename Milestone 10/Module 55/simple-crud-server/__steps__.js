a195c926252fe1dca20fcec9da2af92ceff4ed76
Maysha-Khanom-Moon <maysha412@gmail.com>
2025-07-18 23:12:31 +0600

    Overall steps of MongoDB Connections

---
+++ Milestone 10/Module 55/simple-crud-server/__steps__.js
@@ -0,0 +1,18 @@
+/**
+ * ----------------------------------
+ * MongoDB Connection
+ * ----------------------------------
+ * 1. create account on MongoDB Atlas
+ * 2. create a user with password
+ * 3. white list your IP address
+ * 4. database > connect > driver > Node > show all code
+ * 5. adjust the uri
+ * 
+ * 6. CRUD operations: mongodb.com/docs/drivers/node/current/fundamentals/crud/
+ *    - Create: insertOne, insertMany
+ *    - Read: find, findOne
+ *   - Update: updateOne, updateMany
+ *   - Delete: deleteOne, deleteMany
+ * 
+ * # Make the functions async to use await to avoid callback hell
+ */
